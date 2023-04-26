ffcb349e5b252554fedc2e06007d7c9a2f833fc6
Tóth Károly Dávid <j70kew@inf.elte.hu>
2023-04-26 18:52:36 +0200

    Bootstrap UI és fontawsome hozzáadása

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,41 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', function () {
+    return view('posts.index');
+});
+
+Route::get('/posts/create', function () {
+    return view('posts.create');
+});
+
+Route::get('/posts/x', function () {
+    return view('posts.show');
+});
+
+Route::get('/posts/x/edit', function () {
+    return view('posts.edit');
+});
+
+// -----------------------------------------
+
+Route::get('/categories/create', function () {
+    return view('categories.create');
+});
+
+Route::get('/categories/x', function () {
+    return view('categories.show');
+});
+
+// -----------------------------------------
+
+Auth::routes();
